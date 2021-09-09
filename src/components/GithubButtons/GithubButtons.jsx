379e825fe49692fe17e30dfbe1f26f1b6f87a85d
kskeerthi <sk.keetu@gmail.com>
2021-09-09 17:37:13 +0530

    initial draft

---
+++ src/components/GithubButtons/GithubButtons.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import GitHubButton from 'react-github-btn';
+
+const GithubButton = () => (
+  <>
+    <GitHubButton
+      className="github-button"
+      href="https://github.com/kskeerthi"
+      data-icon="octicon-repo-forked"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Fork kskeerthi on GitHub"
+    >
+      Fork
+    </GitHubButton>
+    <GitHubButton
+      className="github-button"
+      href="https://github.com/kskeerthi"
+      data-icon="octicon-star"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Star kskeerthi on GitHub"
+    >
+      Star
+    </GitHubButton>
+  </>
+);
+
+export default GithubButton;
